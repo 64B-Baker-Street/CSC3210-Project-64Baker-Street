d0cff91142e779db04cfd33a9a2fc586cb7ee2f7
Manuel Lopez <mlopez44@student.gsu.edu>
2020-02-09 23:40:12 -0500

    Alex Project 1 code 2

--- First/first.s
+++ First/first.s
@@ -1,12 +1,15 @@
 @ first program
+@Manuel Lopez
 .section .data
 .section .text
 .globl _start
 _start:
-	mov r1, #5
-	sub r1, r1, #1
-	add r1, r1, #4
 
-	mov r7, #1
-	svc #0
+mov r1, #5
+sub r1, r1, #1
+add r1, r1, #4
+
+mov r7, #1
+svc #0
+
 .end
